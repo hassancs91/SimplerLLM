6302401f7579f9a4365bee1eb18634b0c237fca9
Hasan Aboul Hasan <hassan.cs91@gmail.com>
2024-01-18 14:52:48 +0200

    Update Setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='SimplerLLM',  # Replace with your library's name
-    version='0.1.0',  # Your library's initial version
+    version='0.1.1',  # Your library's initial version
     author='LearnWithHasan',  # Your name or your organization's name
     author_email='hasan@learnwithhasan.com',  # Your contact email
     description='An asynchronous text generation library using OpenAI.',  # A short description
@@ -10,11 +10,11 @@ setup(
     long_description_content_type='text/markdown',  # Type of the long description
     url='https://github.com/yourusername/text_generator',  # Link to your project's repository
     packages=find_packages(),  # Automatically find your package
-    install_requires=[
-        'openai>=0.2.4',  # Ensure you pin down the versions for better reproducibility
-        'python-dotenv>=0.15.0',
-        'asyncio',
-    ],
+    # install_requires=[
+    #     'openai>=0.2.4',  # Ensure you pin down the versions for better reproducibility
+    #     'python-dotenv>=0.15.0',
+    #     'asyncio',
+    # ],
     classifiers=[
         'Development Status :: 3 - Alpha',  # Choose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
         'Intended Audience :: Developers',
