3d2d8d1259d31f915fa3599951a914dead112954
Husein <husein.70821@hotmail.com>
2025-01-17 11:43:26 +0200

    Add Gemini Cache

--- SimplerLLM/language/llm.py
+++ SimplerLLM/language/llm.py
@@ -5,6 +5,9 @@ import SimplerLLM.language.llm_providers.ollama_llm as ollama_llm
 import SimplerLLM.language.llm_providers.lwh_llm as lwh_llm
 from SimplerLLM.prompts.messages_template import MessagesTemplate
 from enum import Enum
+import base64
+import requests
+import json
 import os
 
 
@@ -229,20 +232,54 @@ class GeminiLLM(LLM):
         model_messages.extend(messages)
         return model_messages
 
+    def create_cache(
+        self,
+        cached_input: str,
+        ttl: int = 600,
+        ) -> str:
+
+        encoded_content = base64.b64encode(cached_input.encode()).decode()
+        cache_url = f"https://generativelanguage.googleapis.com/v1beta/cachedContents?key={self.api_key}"
+
+        cache_payload = {
+            "model": f"models/{self.model_name}",
+            "contents": [
+                {
+                    "parts": [
+                        {
+                            "inline_data": {
+                                "mime_type": "text/plain",
+                                "data": encoded_content
+                            }
+                        }
+                    ],
+                    "role": "user"
+                }
+            ],
+            "ttl": f"{ttl}s" 
+        }
+
+        response = requests.post(cache_url, headers={"Content-Type": "application/json"}, data=json.dumps(cache_payload))
+        response.raise_for_status()
+
+        cache_id = response.json().get("name")
+        return cache_id
 
 
     def generate_response(
         self,
-        model_name=None,
-        prompt=None,
-        messages=None,
-        system_prompt="You are a helpful AI Assistant",
-        temperature=0.7,
-        max_tokens=300,
-        top_p=1.0,
-        full_response=False,
+        model_name: str =None,
+        prompt: str = None,
+        messages: list = None,
+        system_prompt: str="You are a helpful AI Assistant",
+        temperature: float=0.7,
+        max_tokens: int=300,
+        top_p: float=1.0,
+        full_response: bool=False,
+        prompt_caching: bool = False,
+        cache_id: str = None,
     ):
-        
+ 
         """
         Generate a response using the Gemini model.
 
@@ -283,24 +320,29 @@ class GeminiLLM(LLM):
         params.update(
             {
                 "api_key": self.api_key,
-                "messages": model_messages,
                 "system_prompt": system_prompt,
+                "messages": model_messages,
                 "max_tokens": max_tokens,
                 "full_response": full_response,
+                "prompt_caching": prompt_caching,
+                "cache_id": cache_id,
             }
         )
+
         return gemini_llm.generate_response(**params)
 
     async def generate_response_async(
         self,
-        model_name=None,
-        prompt=None,
-        messages=None,
-        system_prompt="You are a helpful AI Assistant",
-        temperature=0.7,
-        max_tokens=300,
-        top_p=1.0,
-        full_response=False,
+        model_name: str =None,
+        prompt: str = None,
+        messages: list = None,
+        system_prompt: str="You are a helpful AI Assistant",
+        temperature: float=0.7,
+        max_tokens: int=300,
+        top_p: float=1.0,
+        full_response: bool=False,
+        prompt_caching: bool = False,
+        cache_id: str = None,
     ):
         """
         Asynchronously generate a response from the Gemini model.
@@ -344,12 +386,15 @@ class GeminiLLM(LLM):
         params.update(
             {
                 "api_key": self.api_key,
-                "messages": model_messages,
                 "system_prompt": system_prompt,
+                "messages": model_messages,
                 "max_tokens": max_tokens,
                 "full_response": full_response,
+                "prompt_caching": prompt_caching,
+                "cache_id": cache_id,
             }
         )
+
         return await gemini_llm.generate_response_async(**params)
 
 class AnthropicLLM(LLM):
